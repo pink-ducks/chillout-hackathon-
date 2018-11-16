be17b7e4a7a88868faf5d90e1d5ea6e8625201fb
tomaszewskipablo <32485994+tomaszewskipablo@users.noreply.github.com>
2018-11-16 20:02:07 +0100

    add Runner.h

---
+++ hackathon/hackathon/Runner.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<string>
+
+class Runner {
+	std::string name;
+	bool isMan;
+	int age;
+
+	Run aim;
+	Run km10Record = 10000;
+
+	// int index; // how good runner is
+public:
+	
+
+}
