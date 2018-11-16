6279ab2990771e6593ff8ea7d4325048aa23e1db
tomaszewskipablo <32485994+tomaszewskipablo@users.noreply.github.com>
2018-11-16 20:35:04 +0100

    add Authors

---
+++ hackathon/hackathon/Authors.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace hackathon {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Authors
+	/// </summary>
+	public ref class Authors : public System::Windows::Forms::Form
+	{
+	public:
+		Authors(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Authors()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Authors
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(455, 380);
+			this->Name = L"Authors";
+			this->Text = L"Authors";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
