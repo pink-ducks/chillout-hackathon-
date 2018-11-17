6314e0006afe8fe86f2e02fb48132ebdc51398c2
Jakub Głatki <jakub.glatki@gmail.com>
2018-11-17 07:04:36 +0100

    added save to files again

--- hackathon/hackathon/TrainingElements.cpp
+++ hackathon/hackathon/TrainingElements.cpp
@@ -14,6 +14,6 @@ void TrainingElements::makePhase(Runner &runner)
 	Run treshold{runner.index - 18, ((80.0 + runner.index * 0.4) / (runner.index - 7)) - 0.2, 1 };
 	Run longRun{ runner.index - 15, ((80.0 + runner.index * 0.4) / (runner.index - 7)) - 0.2, 1 };
 
-	if (runner.trainingDaysAWeek == 2){
-		base1[1] =   
+	//if (runner.trainingDaysAWeek == 2){
+		//base1[1] =   
 }
