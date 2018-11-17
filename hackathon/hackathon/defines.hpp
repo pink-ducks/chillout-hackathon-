739d8b4392339174b641856f91a4d147e745c205
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2018-11-17 05:02:25 +0100

    Week structure

---
+++ hackathon/hackathon/defines.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+struct Week
+{
+	string monday;
+	string tuesday;
+	string wednesday;
+	string thursday;
+	string friday;
+	string saturday;
+	string sunday;
+};
