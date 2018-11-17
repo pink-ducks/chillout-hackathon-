2d827e57c4b542e9c5d8d4be3111050d662b2b83
Jakub Głatki <jakub.glatki@gmail.com>
2018-11-17 01:28:24 +0100

    getters

--- hackathon/hackathon/Runner.cpp
+++ hackathon/hackathon/Runner.cpp
@@ -12,3 +12,29 @@ isMale(isMale), age(age), distance(distance), time(time), target(target)
 Runner::~Runner()
 {
 }
+
+//getters
+bool Runner::getIsMale() const
+{
+	return isMale;
+}
+
+int Runner::getAge() const
+{
+	return age;
+}
+
+int Runner::getDisctance() const
+{
+	return distance;
+}
+
+double Runner::getTime() const
+{
+	return time;
+}
+
+double Runner::getTarget() const
+{
+	return target;
+}
--- hackathon/hackathon/Runner.h
+++ hackathon/hackathon/Runner.h
@@ -15,4 +15,10 @@ public:
 	Runner();
 	Runner(std::string, bool, int, int, double, double);
 	~Runner();
+
+	bool getIsMale() const;
+	int getAge() const;
+	int getDisctance() const;
+	double getTime() const;
+	double getTarget() const;
 };
