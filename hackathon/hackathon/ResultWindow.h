794fca7812e53bfb177e36f43b4b0f15b78d5f18
Mateusz Piwowarski <matpiwowarski7@gmail.com>
2018-11-17 02:10:35 +0100

    week number

--- hackathon/hackathon/ResultWindow.h
+++ hackathon/hackathon/ResultWindow.h
@@ -19,6 +19,7 @@ namespace hackathon {
 		{
 			InitializeComponent();
 			this->CenterToScreen();
+			this->WeekLabel->Text = "Week " + this->weekNumber;
 			//
 			//TODO: Add the constructor code here
 			//
@@ -36,10 +37,14 @@ namespace hackathon {
 			}
 		}
 	private: System::Windows::Forms::Label^  ProjectLabel;
+	private: System::Windows::Forms::Panel^  panel1;
+	private: System::Windows::Forms::Label^  WeekLabel;
+
 	protected:
 
 	private:
 		/// <summary>
+		int weekNumber = 1;
 		/// Required designer variable.
 		/// </summary>
 		System::ComponentModel::Container ^components;
@@ -52,6 +57,9 @@ namespace hackathon {
 		void InitializeComponent(void)
 		{
 			this->ProjectLabel = (gcnew System::Windows::Forms::Label());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->WeekLabel = (gcnew System::Windows::Forms::Label());
+			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// ProjectLabel
@@ -67,16 +75,41 @@ namespace hackathon {
 			this->ProjectLabel->Text = L"Your training plan";
 			this->ProjectLabel->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::SystemColors::HotTrack;
+			this->panel1->Controls->Add(this->WeekLabel);
+			this->panel1->Location = System::Drawing::Point(30, 80);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(800, 381);
+			this->panel1->TabIndex = 2;
+			// 
+			// WeekLabel
+			// 
+			this->WeekLabel->AutoSize = true;
+			this->WeekLabel->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei Light", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->WeekLabel->ForeColor = System::Drawing::SystemColors::Control;
+			this->WeekLabel->Location = System::Drawing::Point(346, 9);
+			this->WeekLabel->Name = L"WeekLabel";
+			this->WeekLabel->Size = System::Drawing::Size(93, 31);
+			this->WeekLabel->TabIndex = 0;
+			this->WeekLabel->Text = L"Week x";
+			this->WeekLabel->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
 			// ResultWindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ScrollBar;
 			this->ClientSize = System::Drawing::Size(860, 486);
+			this->Controls->Add(this->panel1);
 			this->Controls->Add(this->ProjectLabel);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"ResultWindow";
 			this->Text = L"ResultWindow";
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
